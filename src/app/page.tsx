d4f45a0688004313187b5659bb404317226c72c4
as-zini <aszini@naver.com>
2024-08-18 23:58:02 +0900

    홈페이지 UI 생성 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import getProducts, { ProductsParams } from "@/app/actions/getProducts";
+import Container from "@/components/Container";
+import EmptyState from "@/components/EmptyState";
+import ProductCard from "@/components/ProductCard";
 import Image from "next/image";
+import getCurrentUser from "./actions/getCurrentUser";
+import FloatingButton from "@/components/FloatingButton";
 
 interface HomeProps{
   searchParams: ProductsParams
@@ -8,14 +13,33 @@ interface HomeProps{
 
 //파일 이름을 괄호 안에 넣어주면 파일 경로상에서는 아무런 변화가 없지만 명시적으로 파일을 정리해줄 수 있음
 export default async function Home({searchParams}:HomeProps) {
-  console.log(searchParams)
   const products = await getProducts(searchParams)
+  const currentUser = await getCurrentUser();
 
   console.log(products)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>하이</div>
-    </main>
+    <Container>
+      {/* Category &*/}
+      {products?.data.length === 0
+      ?
+      <EmptyState/>
+      :
+      <>
+        <div className="grid gird-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+        {products.data.map((product) => (
+          <ProductCard
+            currentUser={currentUser}
+            key={product.id} //맵메소드로 순회하고 있기 때문에 무언가 하나의 요소를 쓰려면 고유한 값인 Key값이 있어야하므로 key값 설정
+            data={product}
+          />
+        ))}
+          
+        </div>
+      </>}
+      <FloatingButton
+        href="/products/upload"
+      >+</FloatingButton>
+    </Container>
   );
 }
